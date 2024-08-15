917225dc673a4f11cfa7ac34028eb1091bdea100
David Shaw <david.shaw1242@gmail.com>
2024-08-15 11:04:17 -0400

    fixed security errors

--- json.go
+++ json.go
@@ -27,5 +27,9 @@ func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
 		return
 	}
 	w.WriteHeader(code)
-	w.Write(dat)
+	_, writeErr := w.Write(dat)
+	if writeErr != nil {
+		log.Fatalf("Error writing data: %s", writeErr)
+	}
+
 }
